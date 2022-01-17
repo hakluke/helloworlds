6989fa2f23e2d67b46809f3fde84539267053c98
hakluke <13975395+hakluke@users.noreply.github.com>
2022-01-17 10:24:24 +1000

    init

---
+++ compileall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir compiled
+cp helloworld.py ./compiled/
+crystal build helloworld.cr -o ./compiled/helloworldcr
+go build -o ./compiled/helloworldgo ./helloworld.go
+g++ ./helloworld.cpp -o ./compiled/helloworldcpp

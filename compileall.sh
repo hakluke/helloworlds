4a6177dbd35539d01796124da9ffa73f8eb7ee23
Rahul Narsingipyta <75793684+Goofygiraffe06@users.noreply.github.com>
2024-01-09 16:13:12 +0530

    Update compileall.sh
    added compilation for x86 assembly and java

--- compileall.sh
+++ compileall.sh
@@ -7,4 +7,6 @@ go build -o ./compiled/helloworldgo ./helloworld.go
 g++ ./helloworld.cpp -o ./compiled/helloworldcpp
 rustc helloworld.rs -o ./compiled/helloworldrs
 gcc ./helloworld.c -o ./compiled/helloworldc
-
+javac -d ./compiled/ HelloWorld.java 
+nasm -f elf helloworld.asm -o ./compiled/helloworld.o
+ld ./compiled/helloworld.o -o ./compiled/helloworld

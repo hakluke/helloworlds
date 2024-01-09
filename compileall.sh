8d9f480844e94c60c001101f41bbbbe8108cff54
x86trace <raj625707@gmail.com>
2024-01-09 18:01:32 +0530

    Have Some Ocaml Aswell :P

--- compileall.sh
+++ compileall.sh
@@ -7,4 +7,5 @@ go build -o ./compiled/helloworldgo ./helloworld.go
 g++ ./helloworld.cpp -o ./compiled/helloworldcpp
 rustc helloworld.rs -o ./compiled/helloworldrs
 gcc ./helloworld.c -o ./compiled/helloworldc
+ocamlc helloworld.ml -o helloml
 
